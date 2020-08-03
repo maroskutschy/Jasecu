ad720f2ec5bf084987aacfad8901998d0b3a6cdc
iMarosKE <maroskutschy@gmail.com>
2020-08-03 10:36:07 +0200

    fixed problem on MacOs

--- src/test/java/GlueCode/GeneralStepDefinitions.java
+++ src/test/java/GlueCode/GeneralStepDefinitions.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +20,22 @@ public class GeneralStepDefinitions {
 
     @Before
     public void openBrowserWithLink() throws Throwable {
+
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        // Set the notification setting it will override the default setting
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        // Create object of ChromeOption class
+        ChromeOptions options = new ChromeOptions();
+        // Set the experimental option
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("start-maximized");
+        options.addArguments("--disable-notifications");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disabled-new-style-notification");
+        options.addArguments("--allow-silent-push");
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
         switch (browser) {
             case "Firefox" :
@@ -35,15 +52,7 @@ public class GeneralStepDefinitions {
                 } else {
                     System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/" + operatingSystem + "/chromedriver");
                 }
-                // DISABLE CHROME NOTIFICATIONS:
-                // Create object of HashMap Class
-                Map<String, Object> prefs = new HashMap<String, Object>();
-                // Set the notification setting it will override the default setting
-                prefs.put("profile.default_content_setting_values.notifications", 2);
-                // Create object of ChromeOption class
-                ChromeOptions options = new ChromeOptions();
-                // Set the experimental option
-                options.setExperimentalOption("prefs", prefs);
+
                 driver = new ChromeDriver(options);
                 break;
         }
