6252432729fd183cc1dc2da6f684ae34aab32b5c
iMarosKE <maroskutschy@gmail.com>
2020-09-15 19:18:16 +0200

    added wait after login for Percy

--- src/test/java/pages/FacebookWelcomePage.java
+++ src/test/java/pages/FacebookWelcomePage.java
@@ -27,6 +27,7 @@ public class FacebookWelcomePage extends BasicActions {
     }
 
     public FacebookWelcomePage validateFacebookWelcomePage () throws InterruptedException {
+        Thread.sleep(2000);
         checkElementPresent(newsFeed, "newsFeed");
         return this;
     }
